a260316548785073d030607f6bf0654b3db28ab5
GuruprakashS-cit <guruprakash03official@gmail.com>
2024-03-16 11:28:57 +0530

    npm update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 //import { useState } from 'react'
 //import './App.css'
-import Navbar from './Components/Landing/Navbar'
-import Hero from './Components/Landing/Hero'
-import Theme from './Components/Landing/Theme'
-import Routing from './Components/Routing'
-import { BrowserRouter } from 'react-router-dom'
+
 import {Routes,Route} from 'react-router-dom'
 import UserRL from './Components/UserRL'
-import Login from './Components/Login/Login'
 import Features from './Components/Landing/Features'
 import NotFound from './Components/NotFound'
 
@@ -23,7 +18,6 @@ function App() {
       
     <Routes>
       <Route exact path='/' element={<UserRL/>}/>
-      <Route path='/login' element={<Login/>}/>
       <Route path='/features' element={<Features/>}/>
       <Route path='*' element={<NotFound/>}/>
     </Routes> 
